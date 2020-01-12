f7f3c7f686155de78ce72bc4d7f7e933581dc624
TonyGregg <antonygenil@gmail.com>
2020-01-12 08:34:00 -0600

    Initial commit

---
+++ src/main/java/com/genil/apps/contacts/emailupdate/EmailUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.genil.apps.contacts.emailupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genil/apps/contacts/emailupdate/EmailUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genil.apps.contacts.emailupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
